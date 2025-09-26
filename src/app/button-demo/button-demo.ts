80dc022f802f2b46b0145d3c09f69063490aceac
kamilo <kamil.kazmierczak@pm.me>
2025-09-26 11:36:22 +0200

    Added primeNG to starter project

---
+++ src/app/button-demo/button-demo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-button-demo',
+  imports: [ButtonModule],
+  templateUrl: './button-demo.html',
+})
+export class ButtonDemo {
+
+}
