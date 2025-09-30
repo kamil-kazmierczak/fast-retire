821fdb365936a14d7b14917b7eed09bcfc69a193
kamilo <kamil.kazmierczak@pm.me>
2025-09-30 10:17:39 +0200

    Added icons

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuModule } from 'primeng/menu';
+import { PanelMenu } from 'primeng/panelmenu';
 import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterOutlet, MenuModule],
+  imports: [RouterOutlet, PanelMenu],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
-export class MainLayoutComponent {
-  items: MenuItem[] = [
+export class MainLayoutComponent implements OnInit {
+  items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.items = [
     { label: 'Home', icon: 'pi pi-home', routerLink: '/' },
     { label: 'Products', icon: 'pi pi-box', routerLink: '/products' },
   ];
+  }
 
 }
